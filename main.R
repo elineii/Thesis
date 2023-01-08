f8cc5a9565790ff499b733035baf151af1e163e9
Alina <alina.kostromina@gmail.com>
2023-01-09 01:49:36 +0300

    changes in main

--- main.R
+++ main.R
@@ -26,10 +26,17 @@ corr_12 <- 0.4
 corr_13 <- -0.6
 corr_23 <- 0.3
 
-n_array <- c(25, 50, 100, 500, 1000, 5000)  # Число наблюдений
-iter_all <- 1                               # Число итераций
+n_array <- c(
+  #25, 
+  50, 
+  100, 
+  500, 
+  1000 
+  #5000
+  )  # Число наблюдений
+iter_all <- 50                               # Число итераций
 
-for (n in n_array[1:1]) {
+for (n in n_array) {
   for (num_iter in 1:iter_all){
     
     # 1. Генерация данных
@@ -53,12 +60,11 @@ for (n in n_array[1:1]) {
     directory <- glue("results\\exp_{name_exp}\\n_{n}\\")
     dir.create(directory)
     
-    write.csv(full_h, 
-              glue("{directory}\\coefficients_ml.csv"), 
-              row.names=FALSE,
-              append=TRUE)
+    write.csv(coef_cmprobit, 
+              glue("{directory}\\iter_{num_iter}_coefficients_ml.csv"), 
+              row.names=FALSE)
     
-    write.csv(full_h, 
+    write.csv(probs_cmprobit, 
               glue("{directory}\\iter_{num_iter}_probabilities_ml.csv"), 
               row.names=FALSE)
     
@@ -66,3 +72,27 @@ for (n in n_array[1:1]) {
     
   }
 }
+
+#h
+
+#data <- list(x_1 = X_1,
+#             x_2 = X_2,
+#             y = y,
+#             n = dim(X_1)[1],                 # int<lower=0> n
+#             k_1 = dim(X_1)[2],               # int<lower=0> k
+#             k_2 = dim(X_2)[2])
+#
+#model_normal <- stan(file = "Bivariate probit model with normal errors.stan",
+#                     data = data,                # входные данные
+#                     chains = 1,                 # количество выборок из апостериорного распределения
+#                     iter = 2000)                # удвоенный объем выборки из апостериорного распределения
+#posterior_normal <- extract(model_normal)
+#
+#df_normal <- estimations_metrics(df=df_normal, posterior=posterior_normal, k_1=dim(X_1)[2], k_2=dim(X_2)[2])
+#
+#names(df_normal) <- rnames
+#
+#true = c(beta_1, beta_2, sigma[1,2] / (sqrt(sigma[1,1] * sigma[2,2])), rep(0, length(rnames)-length(beta_1)-length(beta_2)-1))
+#
+#table = data.frame(true=true,
+#                   normal=colMeans(df_normal))
